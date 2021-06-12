ac999a94f15dbb319d9e411d8d1e4267c0aadfe1
a0972199950 <a0972199950@gmail.com>
2021-06-12 14:14:02 +0800

    Use personal NPM for demo

--- demos/react-demo/src/index.js
+++ demos/react-demo/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { applyPolyfills, defineCustomElements } from './loader'
+import { applyPolyfills, defineCustomElements } from 'john-test-design-system/loader'
 
 ReactDOM.render(
   <React.StrictMode>
